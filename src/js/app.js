6d791ce50dbbdfef3d07b313ccdc482a04316456
migueran <mrando.via@gmail.com>
2017-01-17 20:43:39 -0300

    add more languajes, add directive

--- src/js/app.js
+++ src/js/app.js
@@ -1,83 +1,140 @@
-var app = angular.module("app", []);
+var app = angular.module("app", [])
+.directive('textBox', function(){
+    return {
+        restrict: 'E',
+        transclude: true,
+        /*scope:{
+            txtN = '=txtN'
+        },*/
+        template:'<span ng-transclude></span>'+
+        '<span ng-Show="false" class="icoEdit">t</span>'+
+        '<div ng-Show="false">t</div>' 
+    }
+})
 
 //servicio
     app.service('myService', function(){
         this.getService = function(){
         return [
                 {
-                    id : "t00001txt01",
+                    id: "t00001txt01",
                     es: "Esta Historia de coraje y peligo en los remotos confines del universo, en la que un puñado de valientes busca la salvación del cosmos acechados por las legiones del mal, comienza cuando una luz cayo del cielo a TUBUL-TUB, un planeta que hasta la leyenda a olvidado...", 
-                    en: "This story of courage and danger in the far reaches of the universe, in which a handful of brave men seek the salvation of the cosmos stalked by the legions of evil, begins when a light falls from the sky to TUBUL-TUB, a planet that even the legend has forgotten..."
+                    en: "This story of courage and danger in the far reaches of the universe, in which a handful of brave men seek the salvation of the cosmos stalked by the legions of evil, begins when a light falls from the sky to TUBUL-TUB, a planet that even the legend has forgotten...",
+                    fr: "Merci pour l'aide à la traduction. Cliquez sur l'icône pour démarrer.",
+                    it: "Grazie per averci aiutato nella traduzione. Fare clic sull'icona per iniziare.",
+                    pt: "Obrigado por ajudar na tradução. Clique no ícone para iniciar."
                 },
                 {
                     id: "t00002txt01",
                     es: "Los tubulitas eran muy curiosos y fueron a husmear", 
-                    en: "Los tubulitas eran muy curiosos y fueron a husmear"
+                    en: "The tubulits were very curious and went to sniff around",
+                    fr: "Merci pour l'aide à la traduction. Cliquez sur l'icône pour démarrer.",
+                    it: "Grazie per averci aiutato nella traduzione. Fare clic sull'icona per iniziare.",
+                    pt: "Obrigado por ajudar na tradução. Clique no ícone para iniciar."
                 },
                 {
                     id: "t00003txt01",
                     es: "¡M-M-Mirad, hermanitos!", 
-                    en: "¡M-M-Mirad, hermanitos!"
+                    en: "¡L-L-Look, brothers!",
+                    fr: "Merci pour l'aide à la traduction. Cliquez sur l'icône pour démarrer.",
+                    it: "Grazie per averci aiutato nella traduzione. Fare clic sull'icona per iniziare.",
+                    pt: "Obrigado por ajudar na tradução. Clique no ícone para iniciar."
                 },
                 {
                     id: "t00004txt01",
                     es: "¡Una nave eesacial! ¡Porlo menos hace doscientos que no veía una!", 
-                    en: "¡Una nave eesacial! ¡Porlo menos hace doscientos que no veía una!"
+                    en: "¡Una nave eesacial! ¡Porlo menos hace doscientos que no veía una!",
+                    fr: "Merci pour l'aide à la traduction. Cliquez sur l'icône pour démarrer.",
+                    it: "Grazie per averci aiutato nella traduzione. Fare clic sull'icona per iniziare.",
+                    pt: "Obrigado por ajudar na tradução. Clique no ícone para iniciar."
                 },
                 {
                     id: "t00005txt01",
                     es: "Tal vez su tripulnte esté mal herido! ¡Vamos a ver!", 
-                    en: "Tal vez su tripulnte esté mal herido! ¡Vamos a ver!"
+                    en: "Tal vez su tripulnte esté mal herido! ¡Vamos a ver!",
+                    fr: "Merci pour l'aide à la traduction. Cliquez sur l'icône pour démarrer.",
+                    it: "Grazie per averci aiutato nella traduzione. Fare clic sull'icona per iniziare.",
+                    pt: "Obrigado por ajudar na tradução. Clique no ícone para iniciar."
                 },
                 {
                     id: "t00005txt02",
                     es: "¡Ŕápido!", 
-                    en: "¡Ŕápido!"
+                    en: "¡Ŕápido!",
+                    fr: "Merci pour l'aide à la traduction. Cliquez sur l'icône pour démarrer.",
+                    it: "Grazie per averci aiutato nella traduzione. Fare clic sull'icona per iniziare.",
+                    pt: "Obrigado por ajudar na tradução. Clique no ícone para iniciar."
                 },
                 {
                     id: "t00006txt01",
                     es: "¡mmm... etá muy oscuro! ¡Mejor voy a...", 
-                    en: "¡mmm... etá muy oscuro! ¡Mejor voy a..."
+                    en: "¡mmm... etá muy oscuro! ¡Mejor voy a...",
+                    fr: "Merci pour l'aide à la traduction. Cliquez sur l'icône pour démarrer.",
+                    it: "Grazie per averci aiutato nella traduzione. Fare clic sull'icona per iniziare.",
+                    pt: "Obrigado por ajudar na tradução. Clique no ícone para iniciar."
                 },
                 {
                     id: "t00007txt01",
                     es: "¡DETENEOS!", 
-                    en: "¡DETENEOS!"
+                    en: "¡DETENEOS!",
+                    fr: "Merci pour l'aide à la traduction. Cliquez sur l'icône pour démarrer.",
+                    it: "Grazie per averci aiutato nella traduzione. Fare clic sull'icona per iniziare.",
+                    pt: "Obrigado por ajudar na tradução. Clique no ícone para iniciar."
                 },
                 {
                     id: "t00007txt02",
                     es: "¿Acaso sois esbirros del Barón Eclipse? An malherido, Mog Tagor podrá con vosotros!", 
-                    en: "¿Acaso sois esbirros del Barón Eclipse? An malherido, Mog Tagor podrá con vosotros!"
+                    en: "¿Acaso sois esbirros del Barón Eclipse? An malherido, Mog Tagor podrá con vosotros!",
+                    fr: "Merci pour l'aide à la traduction. Cliquez sur l'icône pour démarrer.",
+                    it: "Grazie per averci aiutato nella traduzione. Fare clic sull'icona per iniziare.",
+                    pt: "Obrigado por ajudar na tradução. Clique no ícone para iniciar."
                 },
                 {
                     id: "t00008txt01",
                     es: "¡T-Tranquilo! ¡Somos amigos!", 
-                    en: "¡T-Tranquilo! ¡Somos amigos!"
+                    en: "¡T-Tranquilo! ¡Somos amigos!",
+                    fr: "Merci pour l'aide à la traduction. Cliquez sur l'icône pour démarrer.",
+                    it: "Grazie per averci aiutato nella traduzione. Fare clic sull'icona per iniziare.",
+                    pt: "Obrigado por ajudar na tradução. Clique no ícone para iniciar."
                 },
                 {
                     id: "t00009txt01",
                     es: "¡Entonces decidme donde está a Estrellazul!", 
-                    en: "¡Entonces decidme donde está a Estrellazul!"
+                    en: "¡Entonces decidme donde está a Estrellazul!",
+                    fr: "Merci pour l'aide à la traduction. Cliquez sur l'icône pour démarrer.",
+                    it: "Grazie per averci aiutato nella traduzione. Fare clic sull'icona per iniziare.",
+                    pt: "Obrigado por ajudar na tradução. Clique no ícone para iniciar."
                 },
                 {
                     id: "t00009txt02",
                     es: "¿Estrellazul? ¿Que es eso?", 
-                    en: "¿Estrellazul? ¿Que es eso?"
+                    en: "¿Estrellazul? ¿Que es eso?",
+                    fr: "Merci pour l'aide à la traduction. Cliquez sur l'icône pour démarrer.",
+                    it: "Grazie per averci aiutato nella traduzione. Fare clic sull'icona per iniziare.",
+                    pt: "Obrigado por ajudar na tradução. Clique no ícone para iniciar."
                 },
                 {
                     id: "t00010txt01",
                     es: "¡Tal vez sólo un sueño! ¿Pero... qué sería de nosotros sin sueños? ¡Escuchad y sabréis!", 
-                    en: ""
+                    en: "",
+                    fr: "Merci pour l'aide à la traduction. Cliquez sur l'icône pour démarrer.",
+                    it: "Grazie per averci aiutato nella traduzione. Fare clic sull'icona per iniziare.",
+                    pt: "Obrigado por ajudar na tradução. Clique no ícone para iniciar."
                 },
                 {
                     id: "t00011txt01",
                     es: "Pero alguien merodeaba cerca de la astronave...", 
-                    en: ""
+                    en: "",
+                    fr: "Merci pour l'aide à la traduction. Cliquez sur l'icône pour démarrer.",
+                    it: "Grazie per averci aiutato nella traduzione. Fare clic sull'icona per iniziare.",
+                    pt: "Obrigado por ajudar na tradução. Clique no ícone para iniciar."
                 },
                 {
                     id: "t00012txt01",
                     es: "¡Esta vez no escaparás, Mog Tagor! ¡Jo, Jo, Jo!", 
-                    en: ""
+                    en: "",
+                    fr: "Merci pour l'aide à la traduction. Cliquez sur l'icône pour démarrer.",
+                    it: "Grazie per averci aiutato nella traduzione. Fare clic sull'icona per iniziare.",
+                    pt: "Obrigado por ajudar na tradução. Clique no ícone para iniciar."
                 }
             ]
         }
@@ -103,6 +160,15 @@ app.controller("mainController", function($scope, myService){
         var salida = $scope.txtService[n].es;
         }else if(actlang === 'en'){
         var salida = $scope.txtService[n].en;
+        }else if(actlang === 'fr'){
+        var salida = $scope.txtService[n].fr;
+        $scope.$broadcast('setLangChild', actlang);
+        }else if(actlang === 'it'){
+        var salida = $scope.txtService[n].it;
+        $scope.$broadcast('setLangChild', actlang);
+        }else if(actlang === 'pt'){
+        var salida = $scope.txtService[n].pt;
+        $scope.$broadcast('setLangChild', actlang);
         }
         return salida;
     }
@@ -132,4 +198,12 @@ app.controller("mainController", function($scope, myService){
     }
     $scope.setTxt();
 
+});
+
+app.controller("newLangController", function($scope, myService){
+    $scope.$on('setLangChild', function(event, actlang)
+    {
+        $scope.valor = actlang;
+        /*console.log(actlang);*/
+    })
 });
--- src/js/script.js
+++ src/js/script.js
@@ -8,15 +8,15 @@ $(document).ready(function(){
 		}
 	});
 	//margin-top incial
-	var topIni = Math.floor(wHeight * 0.33 - $('header.contentW').height() - $('footer.contentW').height());
-	$(rwId[0]).css('height', Math.min(topIni, 150))
+	var topIni = Math.floor(wHeight * 0.33 - $('header#header').height() - $('footer#footer').height());
+	$(rwId[0]).css('height', $('header#header').height())
 
 	//inicio de la pagina
 	function setPage(){
 		var rwIdlength = rwId.length; 
 		for(i=0; i < rwIdlength; i++){
 			var j = Math.floor($(rwId[i]).offset().top - $(window).scrollTop());
-			if (j < wHeight * 1.5){
+			if (j < wHeight * 1.1){
 				$(rwId[i]).addClass('active');
 			} else{
 				$(rwId[i]).removeClass('active');
