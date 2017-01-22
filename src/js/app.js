dade5a58f878ef2cf9ea369dd8d1d911ddb0950a
migueran <mrando.via@gmail.com>
2017-01-22 19:51:20 -0300

    updated

--- src/js/app.js
+++ src/js/app.js
@@ -4,16 +4,29 @@ var app = angular.module("app", [])
         return: 'AE',
         scope: {  
           value: '@',
-          lang: '@'
+          lang: '@',
+          id: '@'
         },
-        template:'<span>{{value}}</span>'+
-        '<span ng-Show="true" class="icoEdit">{{lang}}</span>'+
-        '<div class="modalView" ng-Show="false">'+
-        't'+
-        '</div>'/*,
-        $scope.infoUser = function(value,lang){
-            alert("Hola " + value + " tu edad es " + lang);
-        }*/
+        controller: 'controllerDir',
+        template:'<span>{{value}}{{welcome}}</span>'+
+        '<div ng-if="!value">'+
+        '<span ng-click="showModal=!showModal" class="icoEdit"> </span>'+
+        '<div class="modalView" ng-Show="showModal">'+
+            '<div class="langTrue">{{langB}}<div></div>{{baseTr[0]}}</div>'+
+            '<span class="fLeft"> << </span> <span class="fRight"> >> </span>'+
+            '<div class="LangActual">{{langA}}<div></div><input value="{{nuevaTr[0]}}"/></div>'+
+            '<button ng-click="showModal=!showModal" class="fRight"> close </button> <button class="fRight"> save </button>'+
+            '</div>'+
+        '</div>',
+        link : function(scope, elem, attrs, ctrl){
+            /*scope.langB = 'lenguaje before';
+            scope.langA = 'lenguaje after';
+            scope.valueB = baseTr[0];
+            scope.valueA = nuevaTr[0];*/
+            if(attrs.value == ''){
+                ctrl.setForm(attrs.id, attrs.lang);
+            }
+        }
     }
 })
 
@@ -22,14 +35,15 @@ var app = angular.module("app", [])
         this.getService = function(){
         return [
                 {  
-                    intro: [{
-                        es : 'Estrellazul apareció en la revista Anteojito en los años 80, con guión y dibujos que Quique Alcatena, como parte de una serie de obras de fantasía y ciencia ficción.',
-                        en : "Estrellazul appeared in the magazine Anteojito in the 80's, with  Quique Alcatena's script and drawings, as part of a series of works of fantasy and science fiction.",
-                        fr : "Estrellazul paru dans le magazine Anteojito dans les années 80, avec le script et les dessins par Quique Alcatena dans le cadre d'une série d'œuvres de fantasy et de science-fiction.",
-                        it : "Estrellazul apparso sulla rivista Anteojito negli anni '80, con la sceneggiatura e disegni Quique Alcatena come parte di una serie di opere di fantasy e fantascienza.",
-                        pt : 'Estrellazul Anteojito apareceu na revista nos anos 80, com roteiro e desenhos de Quique Alcatena como parte de uma série de obras de fantasia e ficção científica.',
-                    }],
-                    id: "t00001txt00",
+                    id: "intro",
+                    es : 'Estrellazul apareció en la revista Anteojito en los años 80, con guión y dibujos que Quique Alcatena, como parte de una serie de obras de fantasía y ciencia ficción.',
+                    en : "Estrellazul appeared in the magazine Anteojito in the 80's, with  Quique Alcatena's script and drawings, as part of a series of works of fantasy and science fiction.",
+                    fr : "Estrellazul paru dans le magazine Anteojito dans les années 80, avec le script et les dessins par Quique Alcatena dans le cadre d'une série d'œuvres de fantasy et de science-fiction.",
+                    it : "Estrellazul apparso sulla rivista Anteojito negli anni '80, con la sceneggiatura e disegni Quique Alcatena come parte di una serie di opere di fantasy e fantascienza.",
+                    pt : 'Estrellazul Anteojito apareceu na revista nos anos 80, com roteiro e desenhos de Quique Alcatena como parte de uma série de obras de fantasia e ficção científica.',
+                },
+                {
+                    id: "t00000txt01",
                     es: "Gracias por ayudarnos con la traducción. Haga clic en el icono para comenzar.", 
                     en: "Thank you for helping us translate. Click the icon to start.",
                     fr: "Merci pour l'aide à la traduction. Cliquez sur l'icône pour démarrer.",
@@ -62,16 +76,16 @@ var app = angular.module("app", [])
                 },
                 {
                     id: "t00004txt01",
-                    es: "¡Una nave eesacial! ¡Porlo menos hace doscientos que no veía una!", 
-                    en: "¡Una nave eesacial! ¡Porlo menos hace doscientos que no veía una!",
+                    es: "¡Una nave espacial! ¡Porlo menos hace doscientos que no veía una!", 
+                    en: "A space ship! At least two hundred years ago I did not see one!",
                     fr: "",
                     it: "",
                     pt: ""
                 },
                 {
                     id: "t00005txt01",
-                    es: "Tal vez su tripulnte esté mal herido! ¡Vamos a ver!", 
-                    en: "Tal vez su tripulnte esté mal herido! ¡Vamos a ver!",
+                    es: "Tal vez su tripulante esté mal herido! ¡Vamos a ver!", 
+                    en: "Maybe your crewmember is badly wounded! We'll go to see!",
                     fr: "",
                     it: "",
                     pt: ""
@@ -79,15 +93,15 @@ var app = angular.module("app", [])
                 {
                     id: "t00005txt02",
                     es: "¡Ŕápido!", 
-                    en: "¡Ŕápido!",
+                    en: "Quickly!",
                     fr: "",
                     it: "",
                     pt: ""
                 },
                 {
                     id: "t00006txt01",
-                    es: "¡mmm... etá muy oscuro! ¡Mejor voy a...", 
-                    en: "¡mmm... etá muy oscuro! ¡Mejor voy a...",
+                    es: "¡mmm... está muy oscuro! ¡Mejor voy a...", 
+                    en: "mmm...! it's very dark! I better go to ...",
                     fr: "",
                     it: "",
                     pt: ""
@@ -95,15 +109,15 @@ var app = angular.module("app", [])
                 {
                     id: "t00007txt01",
                     es: "¡DETENEOS!", 
-                    en: "¡DETENEOS!",
-                    fr: "",
-                    it: "",
-                    pt: ""
+                    en: "STOP!",
+                    fr: "Arrêtez!",
+                    it: "Stop!",
+                    pt: "Pare!"
                 },
                 {
                     id: "t00007txt02",
                     es: "¿Acaso sois esbirros del Barón Eclipse? An malherido, Mog Tagor podrá con vosotros!", 
-                    en: "¿Acaso sois esbirros del Barón Eclipse? An malherido, Mog Tagor podrá con vosotros!",
+                    en: "Are you minions of Baron Eclipse? Yet badly wounded, Mog Tagor will can with you!",
                     fr: "",
                     it: "",
                     pt: ""
@@ -111,7 +125,7 @@ var app = angular.module("app", [])
                 {
                     id: "t00008txt01",
                     es: "¡T-Tranquilo! ¡Somos amigos!", 
-                    en: "¡T-Tranquilo! ¡Somos amigos!",
+                    en: "C-Calm! We are friends!",
                     fr: "",
                     it: "",
                     pt: ""
@@ -119,7 +133,7 @@ var app = angular.module("app", [])
                 {
                     id: "t00009txt01",
                     es: "¡Entonces decidme donde está a Estrellazul!", 
-                    en: "¡Entonces decidme donde está a Estrellazul!",
+                    en: "Then tell me where the Estrellazul is!!",
                     fr: "",
                     it: "",
                     pt: ""
@@ -127,7 +141,7 @@ var app = angular.module("app", [])
                 {
                     id: "t00009txt02",
                     es: "¿Estrellazul? ¿Que es eso?", 
-                    en: "¿Estrellazul? ¿Que es eso?",
+                    en: "Estrellazul? ¿What is that?",
                     fr: "",
                     it: "",
                     pt: ""
@@ -135,7 +149,7 @@ var app = angular.module("app", [])
                 {
                     id: "t00010txt01",
                     es: "¡Tal vez sólo un sueño! ¿Pero... qué sería de nosotros sin sueños? ¡Escuchad y sabréis!", 
-                    en: "",
+                    en: "Maybe just a dream! But ... what would become of us without our dreams? Listen and you will know!",
                     fr: "",
                     it: "",
                     pt: ""
@@ -143,7 +157,7 @@ var app = angular.module("app", [])
                 {
                     id: "t00011txt01",
                     es: "Pero alguien merodeaba cerca de la astronave...", 
-                    en: "Pero alguien merodeaba cerca de la astronave...",
+                    en: "But someone was prowling near the spaceship ...",
                     fr: "",
                     it: "",
                     pt: ""
@@ -151,7 +165,7 @@ var app = angular.module("app", [])
                 {
                     id: "t00012txt01",
                     es: "¡Esta vez no escaparás, Mog Tagor! ¡Jo, Jo, Jo!", 
-                    en: "¡Esta vez no escaparás, Mog Tagor! ¡Jo, Jo, Jo!",
+                    en: "This time you will not escape, Mog Tagor! ¡Jo, Jo, Jo!",
                     fr: "",
                     it: "",
                     pt: ""
@@ -160,18 +174,17 @@ var app = angular.module("app", [])
         }
     });
 
-//creamos nuestro controlador e injectamos tanto la factoría como el servicio
 app.controller("mainController", function($scope, myService){
-    //devolvemos un servicio
+    //servicio
     $scope.txtService = myService.getService();
 
-    $scope.actlang = 'es';
+    $scope.actlang = 'it';
     $scope.sign = 'mrando.via@gmail.com';
-    $scope.percentLangEs = 100;
-    $scope.percentLangEn = 100;
-    $scope.percentLangFr = 0;
-    $scope.percentLangIt = 0;
-    $scope.percentLangPt = 0;
+    $scope.percentLangEs = percentLang('es');
+    $scope.percentLangEn = percentLang('en')
+    $scope.percentLangFr = percentLang('fr')
+    $scope.percentLangIt = percentLang('it')
+    $scope.percentLangPt = percentLang('pt')
     
     $scope.setLang = function(actlang){
         $scope.actlang=actlang;
@@ -179,45 +192,79 @@ app.controller("mainController", function($scope, myService){
         return $scope.actlang;
     }
 
+    function percentLang(lang){
+        var elementos = [];
+        $scope.txtService.forEach(function (elem) {
+          /*return  elem == $scope.txtService[1];*/
+          elementos.push(elem[lang]);        
+        })
+
+
+        var activos = elementos.filter(function(e){return e != '';})
+        var salida = Math.floor(activos.length / elementos.length * 100);
+        return salida;
+    } 
+
+/*    function testJson(element, index) {
+            var value = 't00012txt01';
+            var lang = $scope.actlang;
+            if(element.id == value)
+            {
+                console.log("a[" + index + "] = " + element[lang]);
+                console.log(Object.keys(element) )
+            }
+        }*/
+    /*$scope.txtService.forEach(testJson);*/
 
     var lang = function(n, lang){
-        var actlang = lang;
-        if(actlang === 'es'){
-        var salida = $scope.txtService[n].es;
-        $scope.intro = $scope.txtService[0].intro[0].es;
-        }else if(actlang === 'en'){
-        var salida = $scope.txtService[n].en;
-        $scope.intro = $scope.txtService[0].intro[0].en;
-        }else if(actlang === 'fr'){
-        var salida = $scope.txtService[n].fr;
-        $scope.intro = $scope.txtService[0].intro[0].fr;
-        }else if(actlang === 'it'){
-        var salida = $scope.txtService[n].it;
-        $scope.intro = $scope.txtService[0].intro[0].it;
-        }else if(actlang === 'pt'){
-        var salida = $scope.txtService[n].pt;
-        $scope.intro = $scope.txtService[0].intro[0].pt;
-        }
+        var salida = $scope.txtService[n][lang];
         return salida;
     }
+
     $scope.setTxt = function(){
-        $scope.t00001txt00 = lang(0, $scope.actlang);
-        $scope.t00001txt01 = lang(1, $scope.actlang);
-        $scope.t00002txt01 = lang(2, $scope.actlang);
-        $scope.t00003txt01 = lang(3, $scope.actlang);
-        $scope.t00004txt01 = lang(4, $scope.actlang);
-        $scope.t00005txt01 = lang(5, $scope.actlang);
-        $scope.t00005txt02 = lang(6, $scope.actlang);
-        $scope.t00006txt01 = lang(7, $scope.actlang);
-        $scope.t00007txt01 = lang(8, $scope.actlang);
-        $scope.t00007txt02 = lang(9, $scope.actlang);
-        $scope.t00008txt01 = lang(10, $scope.actlang);
-        $scope.t00009txt01 = lang(11, $scope.actlang);
-        $scope.t00009txt02 = lang(12, $scope.actlang);
-        $scope.t00010txt01 = lang(13, $scope.actlang);
-        $scope.t00011txt01 = lang(14, $scope.actlang);
-        $scope.t00012txt01 = lang(15, $scope.actlang);
+        $scope.intro = lang(0, $scope.actlang);
+        $scope.t00001txt00 = lang(1, $scope.actlang);
+        $scope.t00001txt01 = lang(2, $scope.actlang);
+        $scope.t00002txt01 = lang(3, $scope.actlang);
+        $scope.t00003txt01 = lang(4, $scope.actlang);
+        $scope.t00004txt01 = lang(5, $scope.actlang);
+        $scope.t00005txt01 = lang(6, $scope.actlang);
+        $scope.t00005txt02 = lang(7, $scope.actlang);
+        $scope.t00006txt01 = lang(8, $scope.actlang);
+        $scope.t00007txt01 = lang(9, $scope.actlang);
+        $scope.t00007txt02 = lang(10, $scope.actlang);
+        $scope.t00008txt01 = lang(11, $scope.actlang);
+        $scope.t00009txt01 = lang(12, $scope.actlang);
+        $scope.t00009txt02 = lang(13, $scope.actlang);
+        $scope.t00010txt01 = lang(14, $scope.actlang);
+        $scope.t00011txt01 = lang(15, $scope.actlang);
+        $scope.t00012txt01 = lang(16, $scope.actlang);
     }
     $scope.setTxt();
 
+});
+
+app.controller("controllerDir", function($scope, myService){
+    this.txtService = myService.getService();
+    $scope.baseTr = [];
+    $scope.nuevaTr = [];
+    var completa = [];
+    this.setForm = function(actualId, lang){
+    $scope.welcome = this.txtService[1][lang];
+    this.txtService.forEach(function(elem){
+        if(elem.id === actualId){
+            completa = elem;
+            };
+        });
+    for(var i in completa) {
+            if(completa[i] === actualId){
+            }else if(completa[i] !== ''){
+                $scope.baseTr.push(completa[i]);
+            }else{
+                $scope.nuevaTr.push(completa[i]);
+            };
+        }
+    console.log($scope.baseTr[0])
+    }
+    /*$scope.test(console.log('hola'));*/
 });
